824a243b674e942b040cc1783694e6fe0f5c697f
lbeder <leonid@lbeder.com>
2023-11-01 21:22:00 +0000

    Add comments

--- contracts/UintResolver.sol
+++ contracts/UintResolver.sol
@@ -12,8 +12,12 @@ contract UintResolver is SchemaResolver {
     /// @param value The attested value.
     event Log(uint256 value);
 
+    /// @notice Creates a new UintResolver instance.
     constructor(IEAS eas) SchemaResolver(eas) {}
 
+    /// @notice An example resolver onAttest callback that decodes a uint256 value and just logs it.
+    /// @param attestation The new attestation.
+    /// @return Whether the attestation is valid.
     function onAttest(Attestation calldata attestation, uint256 /*value*/) internal override returns (bool) {
         uint256 value = abi.decode(attestation.data, (uint256));
 
@@ -22,6 +26,8 @@ contract UintResolver is SchemaResolver {
         return true;
     }
 
+    /// @notice An example resolver onRevoke fallthrough callback (which currently doesn't do anything).
+    /// @return Whether the attestation can be revoked.
     function onRevoke(Attestation calldata /*attestation*/, uint256 /*value*/) internal pure override returns (bool) {
         return true;
     }
