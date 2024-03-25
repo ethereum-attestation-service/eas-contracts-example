40c01f98ce4cfcc9dbb6a0ca39127d0430dfca1e
lbeder <leonid@lbeder.com>
2024-03-25 13:58:16 -0400

    Update solidity version

--- contracts/eip712/EIP712Verifier.sol
+++ contracts/eip712/EIP712Verifier.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.22;
+pragma solidity 0.8.23;
 
 import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import { ShortString, ShortStrings } from "@openzeppelin/contracts/utils/ShortStrings.sol";
